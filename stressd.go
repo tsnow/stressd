c6ddfb344015587d4b2d142b63376fb8221b241d
Alan Harris <ah.anachronistic@gmail.com>
2014-01-26 09:07:03 -0500

    Add an application entry point.
    Right now it simply parses flags into the configuration struct and prints a single log line indicating environment, PID and application version.

---
+++ stressd.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"stressd/config"
+)
+
+var conf = config.New()
+
+func init() {
+	flag.StringVar(&conf.Environment, "env", "development", "the runtime environment (production, test, etc.)")
+	flag.BoolVar(&conf.HelpRequested, "help", conf.HelpRequested, "display this help information")
+}
+
+func parseArgs() {
+	flag.Parse()
+	if conf.HelpRequested {
+		flag.Usage()
+		os.Exit(1)
+	}
+	config.Config = conf
+}
+
+func main() {
+	parseArgs()
+	log.Println("stressd [ env:", conf.Environment, "- pid:", os.Getpid(), "- ver:", config.VERSION, "]")
+}
