c6dfa0a29e53ae2a9648303df22ac4a8fd268c01
Alan Harris <ah.anachronistic@gmail.com>
2014-01-26 09:39:21 -0500

    Populate server settings via command line options.
    You could make a solid argument for config files too, I suppose.

--- stressd.go
+++ stressd.go
@@ -12,6 +12,10 @@ var conf = config.New()
 
 func init() {
 	flag.StringVar(&conf.Environment, "env", "development", "the runtime environment (production, test, etc.)")
+	flag.StringVar(&conf.Address, "addr", "0.0.0.0", "the IP address for the web UI")
+	flag.StringVar(&conf.Port, "port", "9898", "the port for the web UI")
+	flag.StringVar(&conf.WebsocketAddress, "wsaddr", "0.0.0.0", "the IP address for the websocket server")
+	flag.StringVar(&conf.WebsocketPort, "wsport", "9899", "the port for the websocket server")
 	flag.BoolVar(&conf.HelpRequested, "help", conf.HelpRequested, "display this help information")
 }
 
