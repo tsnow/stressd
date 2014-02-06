4ef7118997a34b5e4b8324855e93864ef2c1ff0b
Justin Streufert <monitron@gmail.com>
2014-02-05 19:22:48 -0500

    Minor form processing fixage

--- static/js/index.js
+++ static/js/index.js
@@ -11,12 +11,12 @@ StressPlan.prototype = {
             request.ajax();
         },
         parseForm: function(){
-            this.num_requests = this.form.find('input[name="num_requests"]').value();
-            this.num_workers = this.form.find('input[name="num_workers"]').value();
-            this.http_method = this.form.find('input[name="http_method"]').value();
-            this.http_headers = this.form.find('input[name="http_headers"]').value().split("\n");
-            this.url = this.form.find('input[name="url"]').value();
-            this.request_body = this.form.find('input[name="request_body"]').value();
+            this.num_requests = this.form.find('input[name="num_requests"]').val();
+            this.num_workers = this.form.find('input[name="num_workers"]').val();
+            this.http_method = this.form.find('input[name="http_method"]').val();
+            this.http_headers = this.form.find('textarea[name="http_headers"]').val().split("\n");
+            this.url = this.form.find('input[name="url"]').val();
+            this.request_body = this.form.find('textarea[name="request_body"]').val();
         },
         oneRequest: function(){
             return new Request(this,this.url, this.http_method, this.http_headers, this.request_body);
@@ -30,7 +30,8 @@ var Request = function(plan, url, http_method, http_headers, request_body){
 };
 
 $(document).ready( function() {
-    $('#stress-plan').submit(function() {
+    $('#stress-plan').submit(function(event) {
+            event.preventDefault();
             var stressPlan = new StressPlan($(this));
             stressPlan.parseForm();
             stressPlan.buildRequests();
