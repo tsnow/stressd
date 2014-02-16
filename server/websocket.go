687692bffdd37e5a624258fa09d1287ff5cb9187
Tim Snowhite <tsnow@Tims-MacBook-Air.local>
2014-02-16 01:26:02 -0500

    [coder golang] Parse and Build the request and response jsons.

--- server/websocket.go
+++ server/websocket.go
@@ -34,11 +34,33 @@ func handleDisconnects() {
 	}
 }
 
+
+type stressPlanMsg struct {
+	Key string `json:"key"`
+}
+
+type stressResponseMsg struct {
+	Key string `json:"key"`
+}
+
 func websocketHandler(sock *ws.Conn) {
 	log.Print(sock.RemoteAddr(), " connected")
 	connections[sock] = sock.RemoteAddr().String()
 
+	var plan stressPlanMsg
+	ws.JSON.Receive(sock, &plan)
+	log.Print(plan, " received.")
 	for {
 		select {
+		case res :=<- results:
+			message := &stressResponseMsg{
+				Key: "request_complete",
+				Data: res,
+			}
+			log.Print(res, " sent in response.")
+			if ws.Message.Send(sock, message) != nil {
+				deactivateChannel <- sock
+			}
+		}		
 	}
 }
