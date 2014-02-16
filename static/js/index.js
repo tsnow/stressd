2eaaa846c917be962f20ef12f85050913f3adba4
Tim Snowhite <tsnow@Tims-MacBook-Air.local>
2014-02-15 22:29:41 -0500

    [coder golang] Start the golang version of the frontend websocket connection.

--- static/js/index.js
+++ static/js/index.js
@@ -44,6 +44,31 @@ function initFramework(){
             });
         });
     } else {
+      var connection = new WebSocket('ws://' + window.location.host + ':9899/events');
+
+      initStressPlanSubmit(function(data){
+          connection.send({
+            key: 'plan',
+            data: data
+          });
+      });
+      // When the connection is open, send some data to the server
+      connection.onopen = function () {
+        // connection.send('Ping'); // Send the message 'Ping' to the server
+      };
+
+      // Log errors
+      connection.onerror = function (error) {
+        console.log('WebSocket Error ' + error);
+      };
+
+      // Log messages from the server
+      connection.onmessage = function (e) {
+        console.log('Server: ' + e.data);
+        if(e.data.key != undefined && e.data.key == 'request_complete'){
+          updateSummary(e.data.data);
+        }
+      };
     }
 }
 
