926a0523f347f756be9abe91cdfde55268196d8a
Tim Snowhite <tsnow@Tims-MacBook-Air.local>
2014-02-16 13:32:29 -0500

    [coder golang] symlink server/scripts/application.js to coder/static/js/index.js
    - and we no longer need to care about it in coder_to_first_stab.sh

--- coder_to_first_stab.sh
+++ coder_to_first_stab.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-git checkout origin/coder -- static/js/index.js .gitignore
-mv static/js/index.js server/scripts/application.js
-git add server/scripts/application.js
-git rm --cached static/js/index.js
-
-git diff -u "$(cat coder_latest.sha)"..origin/coder -- views/index.html | patch server/html/home.html
-git rev-parse origin/coder > coder_latest.sha
+git diff -u "$(cat coder_latest.sha)"..HEAD -- coder/views/index.html | patch server/html/home.html
+git rev-parse HEAD > coder_latest.sha
 git add server/html/home.html coder_latest.sha
-sed -i"" -e 's/var coder = 1;/var coder = 0;/' server/scripts/application.js
-git add server/scripts/application.js
+
